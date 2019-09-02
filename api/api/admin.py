d7c8309cbce09ef93cacecfb896bbfb0b37be8b0
Andy Wong <alacritythief@gmail.com>
2019-09-02 14:56:15 -0400

    register models with admin for easy data creation

--- api/api/admin.py
+++ api/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Food, Ingredient, RestrictionTag, Recipe
 
 # Register your models here.
+admin.site.register(Food)
+admin.site.register(Ingredient)
+admin.site.register(RestrictionTag)
+admin.site.register(Recipe)
