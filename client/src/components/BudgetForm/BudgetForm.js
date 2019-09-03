b27e8723e8516e4ebec76ed320231516431d78ea
Andy Wong <alacritythief@gmail.com>
2019-09-03 00:27:59 -0400

    inital form

--- client/src/components/BudgetForm/BudgetForm.js
+++ client/src/components/BudgetForm/BudgetForm.js
@@ -3,11 +3,38 @@ import { BudgetContainer } from '../../styled/containers';
 import { PaycheckInput } from '../../styled/forms';
 
 class BudgetForm extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      paycheckValue: ''
+    }
+  }
+
+  changePaycheck = (event) => {
+    let paycheckAmt = event.target.value;
+    this.setState({
+      paycheckValue: paycheckAmt
+    })
+  }
+
+  handlePaycheck = (event) => {
+    if (event.key === 'Enter' && this.state.paycheckValue !== "") {
+      event.currentTarget.blur();
+    }
+  }
+
   render () {
     return (
       <BudgetContainer>
         Weekly Paycheck amount:<br/>
-        <PaycheckInput />
+        <PaycheckInput 
+          type="text"
+          placeholder="Enter weekly or bi-weekly paycheck amount"
+          value={ this.state.payCheckValue }
+          onChange={ this.changePaycheck }
+          onKeyUp={ this.handlePaycheck }
+        />
+        { this.state.paycheckValue }
       </BudgetContainer>
     )
   }
--- client/src/styled/forms.js
+++ client/src/styled/forms.js
@@ -1,4 +1,11 @@
 import styled from 'styled-components';
+import * as c from '../styled/colors';
+// import * as d from '../constants/devices';
 
 export const PaycheckInput = styled.input`
+  border: 1px solid ${c.BLACK};
+  border-radius: 0.2rem;
+  font-size: 1rem;
+  padding: 0.5rem;
+  width: 100%;
 `
